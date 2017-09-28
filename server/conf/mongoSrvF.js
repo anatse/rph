799a5a660c74dc140216ebe3459864f2cfda87c5
anatolse <anatolse@yandex.ru>
2017-09-28 20:59:00 +0300

    fix textData not found error

--- server/conf/mongoSrvF.js
+++ server/conf/mongoSrvF.js
@@ -2,28 +2,29 @@ db.system.js.save ({
     _id: "soundex",
     value: function (word) {
         var metaphone = [
-            [/ЙО|ИО|ЙЕ|ИЕ/g, "И"],
-            [/О|Ы|Я/g, "А"],
-            [/Е|Ё|Э/g, "И"],
-            [/Ю/g, "У"],
-            [/Б/g, "П"],
-            [/З/g, "С"],
-            [/Д/g, "Т"],
-            [/В/g, "Ф"],
-            [/Г/g, "К"],
-            [/ТС|ДС/g, "Ц"],
-            [/Н{2,}/g, "Н"],
-            [/С{2,}/g, "С"],
-            [/Р{2,}/g, "Р"],
-            [/М{2,}/g, "М"],
-            [/[УЕАЫОИЯЮЭ]{1,}$/g, ""]
-        ]
+            [/йо|ио|йе|ие/g, "и"],
+            [/о|ы|я/g, "а"],
+            [/е|ё|э/g, "и"],
+            [/ю/g, "у"],
+            [/б/g, "п"],
+            [/з/g, "с"],
+            [/д/g, "т"],
+            [/в/g, "ф"],
+            [/г/g, "к"],
+            [/тс|дс/g, "ц"],
+            [/н{2,}/g, "н"],
+            [/с{2,}/g, "с"],
+            [/р{2,}/g, "р"],
+            [/м{2,}/g, "м"],
+            [/[уеаыоияюэ]{1,}$/g, ""]
+        ];
 
-        var result = word.toUpperCase();
+        var result = word.toLowerCase();
         metaphone.forEach(function(rule) {
             result = result.replace(rule[0], rule[1])
-        })
-        return result
+        });
+
+        return result;
     }
 });
 
@@ -36,7 +37,7 @@ db.system.js.save ({
             "d": "в", "f": "а", "g": "п", "h": "р", "j": "о", "k": "л", "l": "д",
             ";": "ж", "'": "э", "z": "я", "x": "ч", "c": "с", "v": "м", "b": "и",
             "n": "т", "m": "ь", ",": "б", ".": "ю", "/": "."
-        }
+        };
 
         return str.replace(new RegExp("[A-z\\/,.;\\'\\]\\[]", "g"), function (x) {
             return x == x.toLowerCase() ? replacer[x] : replacer[x.toLowerCase()].toUpperCase();
@@ -50,39 +51,29 @@ db.system.js.save({
         var rusSearch = rusLayout(searchString).toLowerCase();
         var lowerText = textData.toLowerCase();
         var lowerSearchString = searchString.toLowerCase();
-        var textDataWords = lowerText.split (new RegExp("[ ,.]+"));
+        var textDataWords = lowerText.split (/[ ,.]+/g);
+        if (lowerText === lowerSearchString
+            || lowerText === rusSearch
+            || lowerText.indexOf(lowerSearchString) >= 0
+            || lowerText.indexOf(rusSearch) >= 0)
+            return true;
+        else {
+            var sndSearch = soundex (lowerSearchString);
+            var sndRus = soundex (rusSearch);
+            var arrayLength = textDataWords.length;
 
-        function isTextIn (words, word) {
-            var wordSoundex = soundex (word);
-            var arrayLength = words.length;
             for (var i = 0; i < arrayLength; i++) {
-                var sndWord = soundex (words[i]);
-                if (sndWord === wordSoundex
-                    || sndWord.indexOf(wordSoundex) >= 0)
+                var sndWord = soundex (textDataWords[i]);
+                if (sndWord === sndSearch || sndWord.indexOf(sndSearch) >= 0
+                    || sndWord === sndRus || sndWord.indexOf(sndRus) >= 0)
                     return true;
             }
 
             return false;
         }
-
-        if (lowerText === lowerSearchString
-            || lowerText.toLowerCase() === rusSearch
-            || lowerText.indexOf(lowerSearchString) >= 0
-            || lowerText.indexOf(rusSearch) >= 0)
-            return true;
-        else {
-            var sndText = soundex(lowerText);
-            var sndSearch = soundex (lowerSearchString);
-            var sndRus = soundex (rusSearch);
-
-            return (
-                sndText === sndSearch
-                || sndText === sndRus
-                || isTextIn(textDataWords, sndSearch)
-                || isTextIn(textDataWords, sndRus)
-            );
-        }
     }
 });
 
 db.loadServerScripts();
+
+db.products.find ({$where: "compareString (this.drugsFullName, 'fcgthby')"}).explain("executionStats");
