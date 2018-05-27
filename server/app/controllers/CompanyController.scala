609ae9ab99596adef34a0652aad8047d9b23fb29
anatolse@yandex.ru <qwer#ty123>
2018-05-27 19:29:21 +0300

    fix errors

--- server/app/controllers/CompanyController.scala
+++ server/app/controllers/CompanyController.scala
@@ -134,9 +134,9 @@ class CompanyController @Inject()(
     * Send email about order brom remote cart. Used by android application
     * @return
     */
-  def remoteCartSend = silhouette.UserAwareAction(parse.json[List[RemoteCart]]).async { implicit request =>
+  def remoteCartSend = silhouette.UserAwareAction(parse.json[RemoteCart]).async { implicit request =>
     val cart = request.body
-    val bodyHtml = views.html.emails.remoteCart(sc.get).body
+    val bodyHtml = views.html.emails.remoteCart(cart).body
     val orderNum = Math.abs(bodyHtml.hashCode() % 1000000)
 
     Future {
@@ -144,7 +144,7 @@ class CompanyController @Inject()(
         subject = s"${orderNum}: " + Messages("email.order.title"),
         from = Messages("email.from"),
         to = Seq(Messages("email.order.perform")),
-        bodyHtml = Some(views.html.emails.remoteCart(cart).body)
+        bodyHtml = Some(bodyHtml)
       ))
     }.map(c => Ok(Json.obj("orderNo" -> orderNum)))
   }
